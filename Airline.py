795c0b7df590f8cff2478a1ab14b975a7d98f76c
chewie83 <pandur.akos@gmail.com>
2025-04-25 22:32:08 +0200

    Update book and unhook method in Airline.py

--- Airline.py
+++ Airline.py
@@ -22,12 +22,24 @@ class Airline:
                   f"Arrival: {flight.arrival_airport}, Departure date: {flight.departure_date}, "
                   f"Price: {flight.price}, Booked: {'Yes' if flight.is_booked else 'No'}")
 
-    def book_by_flight_number(self, flight_number):
+        def book_by_flight_number(self, flight_number):
         for flight in self._flights:
             if flight.flight_number == flight_number:
-                return flight.book_flight()
+                if flight.is_booked:
+                    print("Már foglalt ez a járat.")
+                    return False
+                else:
+                    return flight.book_flight()
+        print("Nincs ilyen járat vagy rosszul adta meg a járatszámot!")
+        return False
 
     def unbook_by_flight_number(self, flight_number):
         for flight in self._flights:
             if flight.flight_number == flight_number:
-                return flight.unbook_flight()
+                if not flight.is_booked:
+                    print("Ez a járat jelenleg nincs lefoglalva.")
+                    return False
+                else:
+                    return flight.unbook_flight()
+        print("Nincs ilyen járat vagy rosszul adta meg a járatszámot!")
+        return False
